2bf13b27348b6f72f3876dd201b3bd89ec5dc68b
Krzysztoff27 <90507134+Krzysztoff27@users.noreply.github.com>
2025-01-30 23:43:59 +0100

    Add files via upload

---
+++ scripts/form_handlers/EditFormHandler.php
@@ -0,0 +1,23 @@
+<?php
+    include_once "../Connection.php";
+    include_once "../Query.php";
+    
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
+        
+        $id = intval($_POST['id']);
+        $connection_read = new Connection("readWorker");
+
+        $stmt = $connection_read->getPDO()->prepare("SELECT * FROM `zawodnicy` WHERE id = :id");
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        $player = Query::formatJSON($stmt);
+    
+        if ($player) {
+            echo $player;
+        } else {
+            echo json_encode(["error" => "Player not found"]);
+        }
+
+        $connection_read -> close();
+    } 
+?>
